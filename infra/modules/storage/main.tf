e231c50d873e59a1f11ee6789dd394ecd48fca90
Hassan Mir <59356530+hassanmirdev@users.noreply.github.com>
2025-02-22 22:53:47 +0530

    Create main.tf

---
+++ infra/modules/storage/main.tf
@@ -0,0 +1,44 @@
+# Bucket 1: create source bucket, This bucket is used for storing the source images for employees
+
+resource "aws_s3_bucket" "src_bucket" {
+  bucket = "buckettostoreemployeeimages"
+}
+
+# ensures that bucket cannot be accessed without proper permissions.
+
+resource "aws_s3_bucket_ownership_controls" "src_bucket_ownership_controls" {
+  bucket = aws_s3_bucket.src_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+# Ensures that the bucket is private.
+
+resource "aws_s3_bucket_acl" "src_bucket_acl" {
+  depends_on = [aws_s3_bucket_ownership_controls.src_bucket_ownership_controls]
+  bucket = aws_s3_bucket.src_bucket.id
+  acl    = "private"
+}
+
+# Bucket 2: create destination bucket, This bucket will be used to store the generated greeting cards.
+
+resource "aws_s3_bucket" "dst_bucket" {
+  bucket = "buckettostoregreetingcards"
+}
+
+# ensures that bucket cannot be accessed without proper permissions.
+
+resource "aws_s3_bucket_ownership_controls" "dst_bucket_ownership_controls" {
+  bucket = aws_s3_bucket.dst_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+# Ensures that the bucket is private.
+
+resource "aws_s3_bucket_acl" "dst_bucket_acl" {
+  depends_on = [aws_s3_bucket_ownership_controls.dst_bucket_ownership_controls]
+  bucket = aws_s3_bucket.dst_bucket.id
+  acl    = "private"
+}
