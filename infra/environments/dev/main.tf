e425c1e340e7b58bf6d1773ac1e9bf943c917d59
Hassan Mir <59356530+hassanmirdev@users.noreply.github.com>
2025-02-22 23:23:53 +0530

    Update main.tf

--- infra/environments/dev/main.tf
+++ infra/environments/dev/main.tf
@@ -2,7 +2,7 @@ module "storage" {
   source          = "../../modules/storage"
   src_bucket_name = "buckettostoreemployeeimages"
   dst_bucket_name = "buckettostoregreetingcards"
-  tag_environment = "dev"
+  tag_environment = var.environment
 }
 
 module "sqs" {
@@ -18,7 +18,7 @@ module "lambda" {
     dst_bucket_id = module.storage.dst_bucket_id
     greeting_queue_arn = module.sqs.greeting_queue_arn
     lambda_memory_size = var.lambda_memory_size
-    tag_environment = "dev"
+    tag_environment = var.environment
 }
 
 module "apigatway" {
@@ -26,7 +26,7 @@ module "apigatway" {
   depends_on = [ module.sqs ]
   greeting_queue_name = module.sqs.greeting_queue_name
   greeting_queue_arn = module.sqs.greeting_queue_arn
-  tag_environment = "dev"
+  tag_environment = var.environment
 }
 
 output "greeting_api_endpoint" {
