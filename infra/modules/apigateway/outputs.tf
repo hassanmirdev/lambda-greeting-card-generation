e1735920973386c7a930e791db63ad31546ab015
Hassan Mir <59356530+hassanmirdev@users.noreply.github.com>
2025-02-23 00:16:14 +0530

    Update outputs.tf

--- infra/modules/apigateway/outputs.tf
+++ infra/modules/apigateway/outputs.tf
@@ -2,3 +2,22 @@
 output "greeting_api_endpoint" {
   value = "${aws_api_gateway_deployment.greeting_api_deployment.invoke_url}/greet"
 }
+# Output the REST API ID
+output "greeting_api_rest_api_id" {
+  value = aws_api_gateway_rest_api.greeting_api.id
+}
+
+# Output the Resource ID for the greet resource
+output "greeting_api_resource_id" {
+  value = aws_api_gateway_resource.greet_resource.id
+}
+
+# Output the Method HTTP Method
+output "greeting_api_method_http" {
+  value = aws_api_gateway_method.greet_method.http_method
+}
+
+# If you want to output the Lambda invoke URI (used in integration)
+output "greeting_lambda_invoke_arn" {
+  value = aws_lambda_function.greeting_lambda.invoke_arn
+}
