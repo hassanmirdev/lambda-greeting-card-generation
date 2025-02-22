9fae887762e0302bbea23762eac4d235d88df69c
Hassan Mir <59356530+hassanmirdev@users.noreply.github.com>
2025-02-22 23:22:47 +0530

    Create main.tf

---
+++ infra/environments/dev/main.tf
@@ -0,0 +1,34 @@
+module "storage" {
+  source          = "../../modules/storage"
+  src_bucket_name = "buckettostoreemployeeimages"
+  dst_bucket_name = "buckettostoregreetingcards"
+  tag_environment = "dev"
+}
+
+module "sqs" {
+  source          = "../../modules/sqs"
+}
+
+module "lambda" { 
+    source = "../../modules/lambda"
+    depends_on = [ module.storage, module.sqs ]
+    src_bucket_arn = module.storage.src_bucket_arn
+    src_bucket_id = module.storage.src_bucket_id
+    dst_bucket_arn = module.storage.dst_bucket_arn
+    dst_bucket_id = module.storage.dst_bucket_id
+    greeting_queue_arn = module.sqs.greeting_queue_arn
+    lambda_memory_size = var.lambda_memory_size
+    tag_environment = "dev"
+}
+
+module "apigatway" {
+  source = "../../modules/api"
+  depends_on = [ module.sqs ]
+  greeting_queue_name = module.sqs.greeting_queue_name
+  greeting_queue_arn = module.sqs.greeting_queue_arn
+  tag_environment = "dev"
+}
+
+output "greeting_api_endpoint" {
+  value = module.apigateway.greeting_api_endpoint
+}
