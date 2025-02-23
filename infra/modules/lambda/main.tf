997e930e4e5fe451275377694ba81c773c488239
Hassan Mir <59356530+hassanmirdev@users.noreply.github.com>
2025-02-23 11:56:20 +0530

    Update main.tf

--- infra/modules/lambda/main.tf
+++ infra/modules/lambda/main.tf
@@ -78,3 +78,38 @@ resource "aws_lambda_function" "greeting_lambda" {
   filename         = data.archive_file.lambda_zip.output_path
   source_code_hash = data.archive_file.lambda_zip.output_base64sha256
 }
+
+
+######## Configuring Lambda event source mapping
+##### The following snippet configures the Lambda function to poll messages from the queue for processing.
+# Create a policy with permissions required to poll messages from an SQS queue
+resource "aws_iam_policy" "greeting_lambda_sqs_policy" {
+  name        = "greeting_lambda_ssqs_policy"
+  description = "Grants access to read messages from SQS"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = ["sqs:ReceiveMessage", "sqs:DeleteMEssage", "sqs:GetQueueAttributes"],
+        Effect = "Allow",
+        Resource = [aws_sqs_queue.greeting_queue.arn]
+      }
+    ]
+  })
+}
+
+# Attach the policy to Lambda execution role created previously
+resource "aws_iam_role_policy_attachment" "greeting_lambda_sqs_policy_attachment" {
+  policy_arn = aws_iam_policy.greeting_lambda_sqs_policy.arn
+  role       = aws_iam_role.lambda_execution_role.name
+}
+
+# Create a Lambda event-source mapping to enable Lambda to poll from the queue
+resource "aws_lambda_event_source_mapping" "greeting_sqs_mapping" {
+  event_source_arn = aws_sqs_queue.greeting_queue.arn
+  function_name    = aws_lambda_function.greeting_lambda.function_name
+  batch_size       = 1
+
+  depends_on = [aws_iam_role_policy_attachment.greeting_lambda_sqs_policy_attachment ]
+}
